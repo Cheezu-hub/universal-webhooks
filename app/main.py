4064b972c2bb2a8b6ffe16ac32ff76e356a5e29d
Cheezu-hub <rajanachaitanya29@gmail.com>
2026-04-26 22:04:49 +0530

    Update CORS to allow Vercel preview deployments with regex

--- app/main.py
+++ app/main.py
@@ -66,11 +66,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
         "http://localhost:8000",
-        "http://127.0.0.1:8000",
-        "https://universal-webhooks.vercel.app"
+        "https://universal-webhooks.vercel.app",
     ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
